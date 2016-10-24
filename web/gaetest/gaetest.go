fc64a05953f40719a4297096884b134e5f0df768
yssk22 <yssk22@gmail.com>
2016-10-24 21:42:22 +0900

    fix compilation error

--- web/gaetest/gaetest.go
+++ web/gaetest/gaetest.go
@@ -10,17 +10,27 @@ import (
 
 var instance aetest.Instance
 
+var once sync.Once
+
 func Instance() aetest.Instance {
-	sync.Once(func() {
-		instance = aetest.NewInstance(&aetest.Options{
+	var err error
+	once.Do(func() {
+		instance, err = aetest.NewInstance(&aetest.Options{
 			AppID: "gaetest",
 			StronglyConsistentDatastore: true,
 		})
+		if err != nil {
+			panic(err)
+		}
 	})
 	return instance
 }
 
 // GET issue a GET request to the test server
 func GET(path string) *web.Request {
-	r := instance.NewRequest("GET", path, nil)
+	r, err := instance.NewRequest("GET", path, nil)
+	if err != nil {
+		panic(err)
+	}
+	return web.NewRequest(r)
 }
