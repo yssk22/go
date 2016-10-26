c6931b4a39eeeb780cd87b1506b61776ae56aaba
yssk22 <yssk22@gmail.com>
2016-10-26 18:17:05 +0900

    test utility for web package

--- x/xtesting/assert/assert.go
+++ x/xtesting/assert/assert.go
@@ -17,13 +17,11 @@ package assert
 
 import (
 	"fmt"
-	"path/filepath"
-	"runtime"
-	"strings"
 	"testing"
 	"time"
 
 	"github.com/speedland/go/x/xreflect"
+	"github.com/speedland/go/x/xruntime"
 )
 
 // Assert is a helper struct for testing assersion
@@ -39,128 +37,131 @@ func New(t *testing.T) *Assert {
 // OK for true assertion.
 func (a *Assert) OK(ok bool, msgContext ...interface{}) {
 	if !ok {
-		a.failure("true", ok, msgContext...)
+		a.Failure("true", ok, msgContext...)
 	}
 }
 
 // Not for false assertion
 func (a *Assert) Not(ok bool, msgContext ...interface{}) {
 	if ok {
-		a.failure("true", ok, msgContext...)
+		a.Failure("true", ok, msgContext...)
 	}
 }
 
 // Nil for nil assertion
 func (a *Assert) Nil(v interface{}, msgContext ...interface{}) {
 	if !xreflect.IsNil(v) {
-		a.failure("<nil>", v, msgContext...)
+		a.Failure("<nil>", v, msgContext...)
 	}
 }
 
 // NotNil for non-nil assertion
 func (a *Assert) NotNil(v interface{}, msgContext ...interface{}) {
 	if xreflect.IsNil(v) {
-		a.failure("<non-nil>", v, msgContext...)
+		a.Failure("<non-nil>", v, msgContext...)
 	}
 }
 
 // Zero for Zero value assertion
 func (a *Assert) Zero(v interface{}, msgContext ...interface{}) {
 	if !xreflect.IsZero(v) {
-		a.failure("<zero>", v, msgContext...)
+		a.Failure("<zero>", v, msgContext...)
 	}
 }
 
 // NotZero for non Zero assertion
 func (a *Assert) NotZero(v interface{}, msgContext ...interface{}) {
 	if xreflect.IsZero(v) {
-		a.failure("<not-zero>", v, msgContext...)
+		a.Failure("<not-zero>", v, msgContext...)
 	}
 }
 
 // EqInt for equality assertion (int)
 func (a *Assert) EqInt(expect, got int, msgContext ...interface{}) {
 	if expect != got {
-		a.failure(expect, got, msgContext...)
+		a.Failure(expect, got, msgContext...)
 	}
 }
 
 // EqInt32 for equality assertion (int32)
 func (a *Assert) EqInt32(expect, got int32, msgContext ...interface{}) {
 	if expect != got {
-		a.failure(expect, got, msgContext...)
+		a.Failure(expect, got, msgContext...)
 	}
 }
 
 // EqInt64 for equality assertion (int64)
 func (a *Assert) EqInt64(expect, got int64, msgContext ...interface{}) {
 	if expect != got {
-		a.failure(expect, got, msgContext...)
+		a.Failure(expect, got, msgContext...)
 	}
 }
 
 // EqFloat32 for equality assertion (float32)
 func (a *Assert) EqFloat32(expect, got float32, msgContext ...interface{}) {
 	if expect != got {
-		a.failure(expect, got, msgContext...)
+		a.Failure(expect, got, msgContext...)
 	}
 }
 
 // EqFloat64 for equality assertion (float64)
 func (a *Assert) EqFloat64(expect, got float64, msgContext ...interface{}) {
 	if expect != got {
-		a.failure(expect, got, msgContext...)
+		a.Failure(expect, got, msgContext...)
 	}
 }
 
 // EqStr for equality assertion (string)
 func (a *Assert) EqStr(expect, got string, msgContext ...interface{}) {
 	if expect != got {
-		a.failure(expect, got, msgContext...)
+		a.Failure(expect, got, msgContext...)
 	}
 }
 
 // EqByteString for equality assertion ([]byte string)
 func (a *Assert) EqByteString(expect string, got []byte, msgContext ...interface{}) {
 	if expect != string(got) {
-		a.failure(expect, got, msgContext...)
+		a.Failure(expect, got, msgContext...)
 	}
 }
 
 // EqTime for equality assertion (time.Time)
 func (a *Assert) EqTime(expect, got time.Time, msgContext ...interface{}) {
 	if !expect.Equal(got) {
-		a.failure(expect, got, msgContext...)
+		a.Failure(expect, got, msgContext...)
 	}
 }
 
 // GtInt for 'greater than' assertion (int)
 func (a *Assert) GtInt(min, got int, msgContext ...interface{}) {
 	if min > got {
-		a.failure(fmt.Sprintf("> %d", min), got, msgContext...)
+		a.Failure(fmt.Sprintf("> %d", min), got, msgContext...)
 	}
 }
 
 // LtInt for 'less than' assertion (int)
 func (a *Assert) LtInt(max, got int, msgContext ...interface{}) {
 	if max < got {
-		a.failure(fmt.Sprintf("< %d", max), got, msgContext...)
+		a.Failure(fmt.Sprintf("< %d", max), got, msgContext...)
 	}
 }
 
-func (a *Assert) failure(expect interface{}, got interface{}, msgContext ...interface{}) {
-	pc, _, _, _ := runtime.Caller(1)
-	testpc, file, line, _ := runtime.Caller(2)
-	file = filepath.Base(file)
-	fun := runtime.FuncForPC(pc)
-	packagepath := strings.Split(runtime.FuncForPC(testpc).Name(), ".")[0]
+// Failure fails the test with a report
+// This function expects to be used by another assertion package, not by test code.
+func (a *Assert) Failure(expect interface{}, got interface{}, msgContext ...interface{}) {
+	frame := xruntime.CaptureStackFrom(2, 1)[0]
+	// pc, _, _, _ := runtime.Caller(1)
+	// testpc, file, line, _ := runtime.Caller(2)
+	// file = filepath.Base(file)
+	// fun := runtime.FuncForPC(pc)
+	// packagepath := strings.Split(runtime.FuncForPC(testpc).Name(), ".")[0]
 
 	report := fmt.Sprintf(
 		"%s failure\n"+
-			"\t source: %s/%s:%d\n"+
+			"\t source: %s:%d\n"+
 			"\t expect: %#v\n"+
 			"\t    got: %#v",
-		fun.Name(), packagepath, file, line, expect, got,
+		frame.FunctionName, frame.ShortFilePath, frame.LineNumber, expect, got,
 	)
 
 	if len(msgContext) > 0 {
