efab8bbf8a7e28c46ecedb54bb1706b296506130
yssk22 <yssk22@gmail.com>
2016-10-26 18:13:52 +0900

    Added StringKeyMap for map[string]interface{}

---
+++ keyvalue/map.go
@@ -0,0 +1,47 @@
+package keyvalue
+
+import "fmt"
+
+// Map is an alias for map[interface{}]interface{} that implements GetterSetter interface
+type Map map[interface{}]interface{}
+
+// Get implements Getter#Get
+func (m Map) Get(key interface{}) (interface{}, error) {
+	if v, ok := m[key]; ok {
+		return v, nil
+	}
+	return nil, KeyError(fmt.Sprintf("%s", key))
+}
+
+// Set implements Setter#Set
+func (m Map) Set(key interface{}, v interface{}) error {
+	m[key] = v
+	return nil
+}
+
+// NewMap returns a new Map (shorthand for `Map(make(map[string]interface{}))`)
+func NewMap() Map {
+	return Map(make(map[interface{}]interface{}))
+}
+
+// StringKeyMap is an alias for map[string]interface{} that implements GetterSetter interface
+type StringKeyMap map[string]interface{}
+
+// Get implements Getter#Get
+func (m StringKeyMap) Get(key interface{}) (interface{}, error) {
+	if v, ok := m[key.(string)]; ok {
+		return v, nil
+	}
+	return nil, KeyError(fmt.Sprintf("%s", key))
+}
+
+// Set implements Setter#Set
+func (m StringKeyMap) Set(key interface{}, v interface{}) error {
+	m[key.(string)] = v
+	return nil
+}
+
+// NewStringKeyMap returns a new Map (shorthand for `Map(make(map[string]interface{}))`)
+func NewStringKeyMap() StringKeyMap {
+	return StringKeyMap(make(map[string]interface{}))
+}
