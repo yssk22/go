88df1e088880f086d48e600e9c50682deed09b26
yssk22 <yssk22@gmail.com>
2016-10-26 20:43:51 +0900

    Added AbsoluteURL func

--- x/xnet/xhttp/xhttp.go
+++ x/xnet/xhttp/xhttp.go
@@ -1,4 +1,38 @@
 // Package xhttp provies utility functions for net/http package
 package xhttp
 
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
+const slash byte = '/'
+
+// AbsoluteURL returns an absolute url from path
+func AbsoluteURL(req *http.Request, path string) string {
+	if strings.HasPrefix(path, "http://") {
+		return path
+	}
+	if strings.HasPrefix(path, "https://") {
+		return path
+	}
+	var scheme, domain string
+	if len(path) == 0 {
+		path = "/"
+	} else {
+		if path[0] != slash {
+			path = path + "/"
+		}
+	}
+
+	u := req.URL
+	if u.Scheme == "" {
+		return path
+	}
+
+	scheme = u.Scheme
+	domain = u.Host
+
+	return fmt.Sprintf("%s://%s%s", scheme, domain, path)
+}
