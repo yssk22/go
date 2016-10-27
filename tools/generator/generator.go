84a010ad6fe88198b951c312d77d93379f129eeb
yssk22 <yssk22@gmail.com>
2016-10-27 23:27:06 +0900

    Return details for compilation error

--- tools/generator/generator.go
+++ tools/generator/generator.go
@@ -12,6 +12,9 @@ import (
 	"go/types"
 	"io"
 	"path/filepath"
+	"strings"
+
+	"github.com/speedland/go/number"
 )
 
 type Generator interface {
@@ -32,7 +35,10 @@ func Run(dir string, g Generator) ([]byte, error) {
 	g.GenSource(&buff)
 	formatted, err := format.Source(buff.Bytes())
 	if err != nil {
-		return buff.Bytes(), fmt.Errorf("generated source cannot not be compiled: %s", err)
+		return nil, &InvalidSourceError{
+			Source: buff.String(),
+			err:    err,
+		}
 	}
 	return formatted, nil
 }
@@ -59,4 +65,43 @@ func parsePackage(dir string) (*build.Package, []*ast.File, error) {
 		return nil, nil, err
 	}
 	return pkg, astFiles, nil
+}
+
+// InvalidSourceError is an error if generated source is unable to compile.
+// Use SourceWithLine() to debug the generated code.
+type InvalidSourceError struct {
+	Source string
+	err    error
+}
+
+// SourceWithLine returns the source code with line numbers
+func (e *InvalidSourceError) SourceWithLine(all bool) string {
+	lines := strings.Split(e.Source, "\n")
+	max := len(fmt.Sprintf("%d", len(lines)))
+	format := fmt.Sprintf("%%%dd: %%s", max+1)
+	errLineFormat := fmt.Sprintf("!%%%dd: %%s", max)
+	n := number.ParseIntOr(strings.Split(e.err.Error(), ":")[0], 0)
+	for i := range lines {
+		if n != 0 && n == i+1 {
+			lines[i] = fmt.Sprintf(errLineFormat, i+1, lines[i])
+		} else {
+			lines[i] = fmt.Sprintf(format, i+1, lines[i])
+		}
+	}
+	if all || n == 0 {
+		return strings.Join(lines, "\n")
+	}
+	n++
+	if n < 5 {
+		lines = lines[:n+4]
+	} else if n < len(lines)-3 {
+		lines = lines[n-6 : n+3]
+	} else {
+		lines = lines[n-6:]
+	}
+	return strings.Join(lines, "\n")
+}
+
+func (e *InvalidSourceError) Error() string {
+	return e.err.Error()
 }
