1ffe42d8004a2b55908571e2f138b3ba468e3689
yssk22 <yssk22@gmail.com>
2016-11-09 15:53:12 +0900

    fix signature

--- number/float_test.go
+++ number/float_test.go
@@ -3,9 +3,9 @@ package number
 import "fmt"
 
 func ExampleParseFloatOr() {
-	fmt.Println(ParseFloatOr("20.1", 1))
-	fmt.Println(ParseFloatOr("20.5a", 1.3))
-	fmt.Println(ParseFloatOr("a", 1.2))
+	fmt.Println(ParseFloat32Or("20.1", 1))
+	fmt.Println(ParseFloat32Or("20.5a", 1.3))
+	fmt.Println(ParseFloat32Or("a", 1.2))
 	// Output:
 	// 20.1
 	// 1.3
