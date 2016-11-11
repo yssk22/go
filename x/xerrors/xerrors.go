501615d67360f1e15021f4a001f6aa36741e4cfd
yssk22 <yssk22@gmail.com>
2016-11-11 15:04:00 +0900

    Added MustNil assersion

--- x/xerrors/xerrors.go
+++ x/xerrors/xerrors.go
@@ -6,3 +6,10 @@ import "fmt"
 func F(format string, v ...interface{}) error {
 	return fmt.Errorf(format, v...)
 }
+
+// MustNil checks err is nil, or panic.
+func MustNil(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
