9d822da0457c7f740f866a9c0b9eebed8a8c1252
yssk22 <yssk22@gmail.com>
2016-11-19 18:28:59 +0900

    Added StubFile

--- x/xnet/xhttp/xhttptest/stub.go
+++ x/xnet/xhttp/xhttptest/stub.go
@@ -4,25 +4,10 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
 )
 
-// rewriter is to rewrite a request not to access a remote resource
-type rewriter struct {
-	mapping map[string]*url.URL
-	base    http.RoundTripper
-}
-
-// RoundTrip implements http.RoundTripper#RoundTrip
-func (r *rewriter) RoundTrip(req *http.Request) (*http.Response, error) {
-	u := req.URL.String()
-	rewriteTo := r.mapping[u]
-	if rewriteTo == nil {
-		return nil, fmt.Errorf("forbitten by xhttptest.Rewriter")
-	}
-	req.URL = rewriteTo
-	return r.base.RoundTrip(req)
-}
-
+// Stub enforce stub accesses for http.Client using URL mapping.
 func Stub(mapping map[string]string, c *http.Client) *http.Client {
 	rewriteMapping := make(map[string]*url.URL)
 	for k, v := range mapping {
@@ -37,4 +22,56 @@ func Stub(mapping map[string]string, c *http.Client) *http.Client {
 		base:    c.Transport,
 	}
 	return c
+}
+
+// rewriter is to rewrite a request not to access a remote resource
+type rewriter struct {
+	mapping map[string]*url.URL
+	base    http.RoundTripper
+}
+
+// RoundTrip implements http.RoundTripper#RoundTrip
+func (r *rewriter) RoundTrip(req *http.Request) (*http.Response, error) {
+	u := req.URL.String()
+	rewriteTo := r.mapping[u]
+	if rewriteTo == nil {
+		return nil, fmt.Errorf("forbitten by xhttptest.Stub")
+	}
+	req.URL = rewriteTo
+	return r.base.RoundTrip(req)
+}
+
+// StubFile is like a Stub but access local file resources
+// instead of making actual HTTP requests
+// Only response body is valid and other fields should not be used when
+// using this function.
+func StubFile(mapping map[string]string, c *http.Client) *http.Client {
+	c.Transport = &fileStub{
+		mapping: mapping,
+	}
+	return c
+}
+
+type fileStub struct {
+	mapping map[string]string
+}
+
+// RoundTrip implements http.RoundTripper#RoundTrip
+func (r *fileStub) RoundTrip(req *http.Request) (*http.Response, error) {
+	u := req.URL.String()
+	filePath := r.mapping[u]
+	if filePath == "" {
+		return nil, fmt.Errorf("forbitten by xhttptest.StubFile")
+	}
+	resp := &http.Response{
+		StatusCode: http.StatusOK,
+		Status:     "OK",
+		Close:      false,
+	}
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("stub file error: %v", err)
+	}
+	resp.Body = file
+	return resp, nil
 }
--- x/xnet/xhttp/xhttptest/stub_test.go
+++ x/xnet/xhttp/xhttptest/stub_test.go
@@ -1,6 +1,7 @@
 package xhttptest
 
 import (
+	"io/ioutil"
 	"net/http"
 	"testing"
 	"x/assert"
@@ -12,4 +13,16 @@ func TestStub(t *testing.T) {
 	_, err := client.Get("http://www.example.com/")
 	a.NotNil(err)
 	a.EqStr("Get http://www.example.com/: forbitten by xhttptest.Rewriter", err.Error())
+}
+
+func TestStubFile(t *testing.T) {
+	a := assert.New(t)
+	client := StubFile(map[string]string{
+		"http://www.example.com/": "./fixtures/stubfile.txt",
+	}, &http.Client{})
+	resp, err := client.Get("http://www.example.com/")
+	a.Nil(err)
+	body, err := ioutil.ReadAll(resp.Body)
+	a.Nil(err)
+	a.EqStr("OK", string(body))
 }
