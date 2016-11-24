ce888a866f41723b37499d81d830f0be3757ce2f
yssk22 <yssk22@gmail.com>
2016-11-24 14:58:34 +0900

    circle ci publish script

---
+++ script/publish.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+CURRENT_DIR=$(dirname $0)
+DIST_DIR="${CURRENT_DIR}/../dist"
+
+GHR=$(which ghr)
+
+if [[ "${GHR}" = "" ]]; then
+    if [[ ! -f "./ghr" ]]; then
+        wget -O ghr.zip https://github.com/tcnksm/ghr/releases/download/v0.5.3/ghr_v0.5.3_linux_amd64.zip 2>/dev/null
+        unzip ghr.zip
+    fi
+    GHR="./ghr"
+fi
+
+VERSION=$(git describe --tags 2>/dev/null || git describe --contains --all HEAD)
+
+${GHR} ${VERSION} ${DIST_DIR}
