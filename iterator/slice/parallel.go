9c010f728eab61ec5a1c5b9ab0a0d145da1cf4bc
yssk22 <yssk22@gmail.com>
2017-04-15 23:40:36 +0900

    Fix zero division

--- iterator/slice/parallel.go
+++ iterator/slice/parallel.go
@@ -41,6 +41,9 @@ func Parallel(list interface{}, option *ParallelOption, fun interface{}) error {
 	if l < n {
 		n = l
 	}
+	if n <= 0 {
+		n = 1
+	}
 
 	errors := SliceError(make([]error, l))
 	eachSize := l / n
