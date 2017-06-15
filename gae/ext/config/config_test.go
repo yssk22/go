39acb0e3f885666b29d053c0a0b551b66e7c2937
yssk22 <yssk22@gmail.com>
2017-06-15 20:23:10 +0900

    adding Service Configuration framework on top of datastore

---
+++ gae/ext/config/config_test.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"os"
+	"testing"
+
+	"google.golang.org/appengine"
+
+	"github.com/speedland/go/gae/gaetest"
+	"github.com/speedland/go/x/xtesting/assert"
+)
+
+func TestMain(m *testing.M) {
+	Global("urlfetch_deadline", "30", "urlfetch deadline secondsa")
+	Global("facebook_app_id", "", "Facebook App ID")
+	os.Exit(gaetest.Run(func() int {
+		return m.Run()
+	}))
+}
+
+func TestConfig_All(t *testing.T) {
+	a := assert.New(t)
+	a.Nil(gaetest.CleanupDatastore(gaetest.NewContext(), "", "myapp"))
+	a.Nil(gaetest.FixtureFromFile(gaetest.NewContext(), "./fixtures/TestConfig.json", nil))
+	appCtx, _ := appengine.Namespace(gaetest.NewContext(), "myapp")
+	c := New()
+	c.Register("myconfig", "mydefaultvalue", "app custom config")
+	c.Register("myconfig2", "mydefaultvalue2", "app custom config2")
+
+	configs := c.All(appCtx)
+	a.EqInt(len(c.defaultKeys), len(configs))
+
+	configMap := make(map[string]*ServiceConfig)
+	for _, cfg := range configs {
+		configMap[cfg.Key] = cfg
+	}
+	a.EqStr("45", configMap["urlfetch_deadline"].Value)
+	a.EqStr("local-app-id", configMap["facebook_app_id"].Value)
+	a.EqStr("datastore value", configMap["myconfig"].Value)
+	a.EqStr("mydefaultvalue2", configMap["myconfig2"].Value)
+}
+
+func TestConfig_Get(t *testing.T) {
+	a := assert.New(t)
+	a.Nil(gaetest.CleanupDatastore(gaetest.NewContext(), "", "myapp"))
+	a.Nil(gaetest.FixtureFromFile(gaetest.NewContext(), "./fixtures/TestConfig.json", nil))
+	appCtx, _ := appengine.Namespace(gaetest.NewContext(), "myapp")
+	c := New()
+	c.Register("myconfig", "mydefaultvalue", "app custom config")
+	c.Register("myconfig2", "mydefaultvalue2", "app custom config2")
+
+	a.EqStr("45", c.Get(appCtx, "urlfetch_deadline").Value)
+	a.EqStr("local-app-id", c.Get(appCtx, "facebook_app_id").Value)
+	a.EqStr("datastore value", c.Get(appCtx, "myconfig").Value)
+	a.EqStr("mydefaultvalue2", c.Get(appCtx, "myconfig2").Value)
+}
+
+func TestConfig_Get_Fallback(t *testing.T) {
+	a := assert.New(t)
+	a.Nil(gaetest.CleanupDatastore(gaetest.NewContext(), "", "myapp"))
+	a.Nil(gaetest.FixtureFromFile(gaetest.NewContext(), "./fixtures/TestConfig.json", nil))
+	appCtx, _ := appengine.Namespace(gaetest.NewContext(), "myapp")
+	c := New()
+	cfg := c.Get(appCtx, "urlfetch_deadline")
+	cfg.Value = "50"
+	c.Set(appCtx, cfg)
+	a.EqStr("50", c.Get(appCtx, "urlfetch_deadline").Value)
+	a.EqStr("45", c.Get(appCtx, "urlfetch_deadline").GlobalValue)
+	a.EqStr("30", c.Get(appCtx, "urlfetch_deadline").DefaultValue)
+}
---
+++ gae/ext/config/service_config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"time"
+
+	"github.com/speedland/go/x/xtime"
+)
+
+// ServiceConfig is a configuration object for a service
+//go:generate ent -type=ServiceConfig
+type ServiceConfig struct {
+	Key       string    `json:"key" ent:"id"`
+	Value     string    `json:"value" ent:"form" datastore:",noindex"`
+	UpdatedAt time.Time `json:"updated_at" ent:"timestamp" datastore:",noindex"`
+
+	Description  string `json:"description" datastore:"-"`
+	DefaultValue string `json:"default_value" datastore:"-"`
+	GlobalValue  string `json:"global_value" datastore:"-"`
+	isGlobal     bool   // if true, ServiceConfig will be stored in a service namespace and global namespace
+}
+
+func newServiceConfig(key string, value string, description string) *ServiceConfig {
+	return &ServiceConfig{
+		Key:         key,
+		Value:       value,
+		Description: description,
+		UpdatedAt:   xtime.Now(),
+	}
+}
