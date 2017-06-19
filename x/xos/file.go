0fcc0a87b1a52f8d38c19716fb67022a7aa1b6d8
yssk22 <yssk22@gmail.com>
2017-06-19 21:39:18 +0800

    use ErrNotExist for stat/path checking

--- x/xos/file.go
+++ x/xos/file.go
@@ -6,23 +6,20 @@ import (
 
 // TryStats is like os.Stat but try multiple paths in order and returns the first found.
 func TryStats(paths ...string) (os.FileInfo, error) {
-	var err error
-	var info os.FileInfo
 	for _, p := range paths {
-		if info, err = os.Stat(p); err == nil {
+		if info, err := os.Stat(p); err == nil {
 			return info, nil
 		}
 	}
-	return nil, err
+	return nil, os.ErrNotExist
 }
 
 // TryExists is like TryStats but returns a path found.
 func TryExists(paths ...string) (string, error) {
-	var err error
 	for _, p := range paths {
-		if _, err = os.Stat(p); err == nil {
+		if _, err := os.Stat(p); err == nil {
 			return p, nil
 		}
 	}
-	return "", err
+	return "", os.ErrNotExist
 }
