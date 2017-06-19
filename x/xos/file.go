69bab5f91ec21a28dbad4252b2da2ebf7a8a0357
yssk22 <yssk22@gmail.com>
2017-06-19 21:35:51 +0800

    added TryExists to check the file path existance simply

--- x/xos/file.go
+++ x/xos/file.go
@@ -9,10 +9,20 @@ func TryStats(paths ...string) (os.FileInfo, error) {
 	var err error
 	var info os.FileInfo
 	for _, p := range paths {
-		info, err = os.Stat(p)
-		if err == nil {
+		if info, err = os.Stat(p); err == nil {
 			return info, nil
 		}
 	}
 	return nil, err
+}
+
+// TryExists is like TryStats but returns a path found.
+func TryExists(paths ...string) (string, error) {
+	var err error
+	for _, p := range paths {
+		if _, err = os.Stat(p); err == nil {
+			return p, nil
+		}
+	}
+	return "", err
 }
