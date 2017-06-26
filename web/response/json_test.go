7172ae31b97cd867425cbdcd7231603837520ccb
yssk22 <yssk22@gmail.com>
2017-06-27 00:00:31 +0900

    json response should return [] if empty.

--- web/response/json_test.go
+++ web/response/json_test.go
@@ -18,3 +18,13 @@ func TestJSON(t *testing.T) {
 
 	a.EqStr("{\"ok\":true}\n", w.Body.String())
 }
+
+func TestJSON_emptySlice(t *testing.T) {
+	a := assert.New(t)
+	var list []int
+	json := NewJSON(list)
+	w := httptest.NewRecorder()
+	json.Render(context.Background(), w)
+
+	a.EqStr("[]\n", w.Body.String())
+}
