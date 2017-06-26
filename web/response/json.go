20c1386f78270ba62c518eb9fb30707790f380df
yssk22 <yssk22@gmail.com>
2017-06-27 00:00:31 +0900

    json response should return [] if empty.

--- web/response/json.go
+++ web/response/json.go
@@ -3,6 +3,7 @@ package response
 import (
 	"encoding/json"
 	"io"
+	"reflect"
 
 	"golang.org/x/net/context"
 )
@@ -10,11 +11,23 @@ import (
 // UseFormattedJSON is a configuration variable about if json object is formatted or not.
 var UseFormattedJSON = false
 
+// UseEmptyIfSliceIsZero is a configuration variable about if the data is an empty slice ([]T{}), then render [] instead of null (Go's default JSON behavior)
+var UseEmptyIfSliceIsZero = true
+
+var emptyBlacket = []byte("[]\n")
+
 type _json struct {
 	data interface{}
 }
 
 func (j _json) Render(ctx context.Context, w io.Writer) {
+	if UseEmptyIfSliceIsZero {
+		v := reflect.ValueOf(j.data)
+		if v.Kind() == reflect.Slice && v.Len() == 0 {
+			w.Write(emptyBlacket)
+			return
+		}
+	}
 	if UseFormattedJSON {
 		buff, err := json.MarshalIndent(j.data, "", "    ")
 		if err != nil {
