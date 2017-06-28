2ff0c67a9eeb6ec965593b31f2a28a7ba927bbd5
yssk22 <yssk22@gmail.com>
2017-06-28 10:03:43 +0900

    don't retry in HTTPFetcher, use retry in retry.Transport instead.

--- crawler/http.go
+++ crawler/http.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"time"
 )
 
 // ErrHTTP is an error when fetch fails.
@@ -20,18 +19,8 @@ func (e *ErrHTTP) Error() string {
 
 // HTTPFetcher is an implementation to fetch a content from an url.
 type HTTPFetcher struct {
-	url        string
-	client     *http.Client
-	MaxRetries int
-	Backoff    func(int, *http.Response, error) time.Duration
-}
-
-// DefaultHTTPMaxRetries is a default value of MaxRetries field for a newly created *HTTPFetcher
-var DefaultHTTPMaxRetries = 3
-
-// DefaultHTTPBackoff is a default value of Backoff function for a newly created *HTTPFetcher
-var DefaultHTTPBackoff = func(i int, resp *http.Response, err error) time.Duration {
-	return time.Duration(0)
+	url    string
+	client *http.Client
 }
 
 // NewHTTPFetcher returns a new *HTTPFetcher for the given url with http.Client.
@@ -41,37 +30,24 @@ func NewHTTPFetcher(url string, client *http.Client) *HTTPFetcher {
 		client = http.DefaultClient
 	}
 	return &HTTPFetcher{
-		url:        url,
-		client:     client,
-		MaxRetries: DefaultHTTPMaxRetries,
-		Backoff:    DefaultHTTPBackoff,
+		url:    url,
+		client: client,
 	}
 }
 
 // Fetch implements Fetcher#Fetch
 func (f *HTTPFetcher) Fetch() (io.Reader, error) {
-	var numTries = 0
-	for {
-		resp, err := f.client.Get(f.url)
-		numTries++
-		if err != nil {
-			if numTries <= f.MaxRetries {
-				time.Sleep(f.Backoff(numTries, resp, err))
-				continue
-			}
-			return nil, err
-		}
-		if resp.StatusCode >= 500 && numTries <= f.MaxRetries {
-			resp.Body.Close()
-			time.Sleep(f.Backoff(numTries, resp, err))
-			continue
-		}
-		var buff bytes.Buffer
-		defer resp.Body.Close()
-		io.Copy(&buff, resp.Body)
-		if resp.StatusCode != 200 {
-			return nil, &ErrHTTP{Response: resp, Content: buff.Bytes()}
-		}
-		return &buff, nil
+	resp, err := f.client.Get(f.url)
+	if err != nil {
+		return nil, err
 	}
+	var buff bytes.Buffer
+	defer resp.Body.Close()
+	if _, err := io.Copy(&buff, resp.Body); err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, &ErrHTTP{Response: resp, Content: buff.Bytes()}
+	}
+	return &buff, nil
 }
