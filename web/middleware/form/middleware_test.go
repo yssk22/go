b62a2af126e776fffbea611fabf46d49f6f0accc
yssk22 <yssk22@gmail.com>
2017-08-17 16:26:38 +0900

    fix to keep to point the correct  req.Request object by WithContext()

--- web/middleware/form/middleware_test.go
+++ web/middleware/form/middleware_test.go
@@ -10,39 +10,47 @@ import (
 	"github.com/speedland/go/web/response"
 )
 
-func TestMiddleware(t *testing.T) {
-	middleware := NewMiddleware(func(v *validator.FormValidator) {
-		v.IntField("param").Required().Min(10)
-	})
+func TestMiddleware_ErrorRequired(t *testing.T) {
 	a := httptest.NewAssert(t)
-	recorder := httptest.NewRecorder(prepareRouter(middleware))
-
+	recorder := httptest.NewRecorder(prepareRouter("param"))
 	res := recorder.TestPost("/form", nil)
 	a.Status(response.HTTPStatusBadRequest, res)
 	var e validator.ValidationError
 	a.JSON(&e, res)
 	a.EqStr("must be required", e.Errors["param"][0].String())
+}
 
-	res = recorder.TestPost("/form", url.Values{
+func TestMiddleware_ErrorMin(t *testing.T) {
+	a := httptest.NewAssert(t)
+	recorder := httptest.NewRecorder(prepareRouter("param"))
+	res := recorder.TestPost("/form", url.Values{
 		"param": []string{"0"},
 	})
 	a.Status(response.HTTPStatusBadRequest, res)
+	var e validator.ValidationError
 	a.JSON(&e, res)
 	a.EqStr("must be more than or equal to 10", e.Errors["param"][0].String())
+}
 
-	res = recorder.TestPost("/form", url.Values{
+func TestMiddleware(t *testing.T) {
+	a := httptest.NewAssert(t)
+	recorder := httptest.NewRecorder(prepareRouter("param"))
+	// should pass validation and response the form value
+	res := recorder.TestPost("/form", url.Values{
 		"param": []string{"10"},
 	})
 	a.Status(response.HTTPStatusOK, res)
-	a.Body("ok", res)
+	a.Body("10", res)
 }
 
-func prepareRouter(middleware *Middleware) *web.Router {
+func prepareRouter(key string) *web.Router {
 	router := web.NewRouter(nil)
 	router.Post("/form",
-		middleware,
+		NewMiddleware(func(v *validator.FormValidator) {
+			v.IntField(key).Required().Min(10)
+		}),
 		web.HandlerFunc(func(req *web.Request, next web.NextHandler) *response.Response {
-			return response.NewText("ok")
+			return response.NewText(req.Form.GetStringOr(key, ""))
 		}))
 	return router
 }
