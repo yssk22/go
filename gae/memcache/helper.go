ccdfc4067ac03e44aac65975914e3dcf5d5f597a
yssk22 <yssk22@gmail.com>
2017-09-11 13:56:47 +0900

    Added cache response

--- gae/memcache/helper.go
+++ gae/memcache/helper.go
@@ -1,8 +1,19 @@
 package memcache
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
 	"time"
+
+	"github.com/speedland/go/x/xlog"
+
+	"github.com/speedland/go/web"
+
+	"github.com/speedland/go/web/response"
 
 	"context"
 )
@@ -31,4 +42,82 @@ func CachedObjectWithExpiration(
 	buff, _ := json.Marshal(gen)
 	json.Unmarshal(buff, dst)
 	return nil
+}
+
+var cachableHeaderKeys = map[string]bool{
+	response.ContentType: true,
+}
+
+func RegisterCachableHeaderKeys(keys ...string) {
+	for _, k := range keys {
+		cachableHeaderKeys[strings.ToLower(k)] = true
+	}
+}
+
+type cachedResponseBody []byte
+
+func (c cachedResponseBody) Render(ctx context.Context, w io.Writer) {
+	w.Write([]byte(c))
+}
+
+const maxCachableBodySize = 1 * 1024 * 900 // 900KB as Memcache supports 1MB
+
+// CacheResponse wraps web.Handler to support cache
+func CacheResponse(name string, h web.Handler) web.Handler {
+	return CacheResponseWithExpire(name, 0, h)
+}
+
+// CacheResponseWithExpire wraps web.Handler to support cache
+func CacheResponseWithExpire(name string, expire time.Duration, h web.Handler) web.Handler {
+	// var ckey = fmt.Sprintf("response-%s", name)
+	type cachedResponse struct {
+		Status response.HTTPStatus `json:"s"`
+		Header http.Header         `json:"h"`
+		Body   cachedResponseBody  `json:"b"`
+	}
+	var ckey = fmt.Sprintf("cache-response-%s", name)
+	var prefix = ckey + " "
+	return web.HandlerFunc(func(req *web.Request, next web.NextHandler) *response.Response {
+		ctx := req.Context()
+		logger := xlog.WithContext(ctx).WithPrefix(prefix)
+		var cr cachedResponse
+		if err := Get(ctx, ckey, &cr); err == nil {
+			if cr.Status != 0 {
+				return &response.Response{
+					Status: cr.Status,
+					Header: cr.Header,
+					Body:   cr.Body,
+				}
+			}
+			logger.Warnf("discard cache as it's invalid: (status: %d, size: %d)", cr.Status, len(cr.Body))
+		}
+
+		resp := h.Process(req, next)
+		if len(resp.Cookies) > 0 {
+			logger.Warnf("disabling cache capability: including cookies")
+			return resp
+		}
+		for k, v := range resp.Header {
+			if _, ok := cachableHeaderKeys[strings.ToLower(k)]; !ok {
+				logger.Warnf("disabling cache capability: including unregistered headres (header-key: %s, header-value: %v)", ckey, k, v)
+				return resp
+			}
+		}
+		var buff bytes.Buffer
+		resp.Body.Render(ctx, &buff)
+		cr = cachedResponse{
+			Status: resp.Status,
+			Header: resp.Header,
+			Body:   cachedResponseBody(buff.Bytes()),
+		}
+		resp.Body = cr.Body
+		if sz := len(cr.Body); sz > maxCachableBodySize {
+			logger.Warnf("disabling cache capability: too big to cache (cache-key:%s, body size: %v)", ckey, sz)
+			return resp
+		}
+		if err := SetWithExpire(ctx, ckey, &cr, expire); err != nil {
+			logger.Errorf("could not use response cache (key: %s): %v", ckey, err)
+		}
+		return resp
+	})
 }
--- gae/memcache/helper_test.go
+++ gae/memcache/helper_test.go
@@ -1,11 +1,16 @@
 package memcache
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
+	"github.com/speedland/go/web"
+	"github.com/speedland/go/web/response"
+
 	"github.com/speedland/go/gae/gaetest"
 	"github.com/speedland/go/x/xtesting/assert"
+	"github.com/speedland/go/x/xtime"
 )
 
 func Test_CachedObjectWithExpiration(t *testing.T) {
@@ -27,4 +32,28 @@ func Test_CachedObjectWithExpiration(t *testing.T) {
 	a.EqStr("10", cachedItem.ID)
 	time.Sleep(2 * time.Second)
 	a.NotNil(Get(gaetest.NewContext(), "foo", &cachedItem))
+}
+
+func Test_CacheResponseWithExpire(t *testing.T) {
+	a := assert.New(t)
+	a.Nil(gaetest.ResetMemcache(gaetest.NewContext()))
+	router := web.NewRouter(web.DefaultOption)
+	expires := 5 * time.Second
+	router.Get("/", CacheResponseWithExpire("mycache", 5*time.Second, web.HandlerFunc(func(req *web.Request, _ web.NextHandler) *response.Response {
+		now := xtime.Now()
+		return response.NewText(fmt.Sprintf("HelloWorld - %s", now))
+	})))
+	recorder := gaetest.NewRecorder(router)
+	resp := recorder.TestGet("/")
+	a.EqInt(200, resp.Code)
+	cachedBody := resp.Body.String()
+
+	resp = recorder.TestGet("/")
+	a.EqInt(200, resp.Code)
+	a.EqStr(cachedBody, resp.Body.String())
+	time.Sleep(expires)
+
+	resp = recorder.TestGet("/")
+	a.EqInt(200, resp.Code)
+	a.Not(cachedBody == resp.Body.String())
 }
