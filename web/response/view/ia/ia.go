20b00fb8005d68ebf345942e6bfeec3e1d5cbe7c
yssk22 <yssk22@gmail.com>
2017-10-03 10:17:12 +0900

    added InstantArticle view

---
+++ web/response/view/ia/ia.go
@@ -0,0 +1,24 @@
+package ia
+
+import (
+	"github.com/speedland/go/web"
+	"github.com/speedland/go/web/response"
+)
+
+type Func func(*web.Request) (*PageVars, error)
+
+// Render implements view.Page#Render
+func (f Func) Render(req *web.Request) *response.Response {
+	v, err := f(req)
+	if err != nil {
+		return response.NewTextWithStatus("internal server error", response.HTTPStatusInternalServerError)
+	}
+	if v == nil {
+		return response.NewTextWithStatus("no content", response.HTTPStatusNoContent)
+	}
+	return response.NewHTMLWithStatus(
+		iaMarkupTemplate,
+		v,
+		response.HTTPStatusOK,
+	)
+}
