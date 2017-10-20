c51386e7599da34bf7f6451255f99a56dd5a9adc
yssk22 <yssk22@gmail.com>
2017-10-20 20:49:12 +0900

    support scraping endpoint

--- services/facebook/facebook_test.go
+++ services/facebook/facebook_test.go
@@ -6,19 +6,38 @@ import (
 
 	"os"
 
-	"github.com/speedland/go/x/xtesting/assert"
 	"context"
+
+	"github.com/speedland/go/x/xtesting/assert"
 )
 
-func Test_GetMe(t *testing.T) {
-	a := assert.New(t)
+func newTestClient(t *testing.T) *Client {
 	token := os.Getenv("TEST_FACEBOOK_ACCESS_TOKEN")
 	if token == "" {
 		t.Skipf("needs TEST_FACEBOOK_ACCESS_TOKEN envvar for this test.")
+		return nil
+	}
+	return NewClient(http.DefaultClient, token)
+}
+
+func Test_GetMe(t *testing.T) {
+	a := assert.New(t)
+	c := newTestClient(t)
+	if c == nil {
 		return
 	}
-	c := NewClient(http.DefaultClient, token)
 	me, err := c.GetMe(context.Background())
 	a.Nil(err)
 	a.OK(me.ID != "")
+}
+
+func Test_ScrapeURL(t *testing.T) {
+	a := assert.New(t)
+	c := newTestClient(t)
+	if c == nil {
+		return
+	}
+	urlobj, err := c.ScrapeURL(context.Background(), "http://www.google.com/")
+	a.Nil(err)
+	a.EqStr("http://www.google.com/", urlobj.URL)
 }
