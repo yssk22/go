67859a8470e8c1d8ad96ab919196b3694384f5a0
yssk22 <yssk22@gmail.com>
2017-10-30 11:21:40 +0900

    support title and description for slideshow

--- services/facebook/publish_video.go
+++ services/facebook/publish_video.go
@@ -3,40 +3,48 @@ package facebook
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"mime/multipart"
+
+	"github.com/speedland/go/x/xtime"
 
 	"github.com/speedland/go/x/xerrors"
 )
 
-type SlideshowParams struct {
+type VideoParams struct {
+	Title                string           `json:"title,omitempty"`
+	Description          string           `json:"description,omitempty"`
+	FileURL              string           `json:"file_url,omitempty"`
+	Published            *bool            `json:"published,omitempty"`
+	ScheduledPublishTime *xtime.Timestamp `json:"scheduled_publish_time,omitempty"`
+	SponsorID            string           `json:"sponsor_id,omitempty"`
+	SlideshowSpec        *SlideshowSpec   `json:"slideshow_spec,omitempty"`
+}
+
+type SlideshowSpec struct {
 	ImagesURLs   []string `json:"images_urls"`
 	DurationMs   int      `json:"duration_ms,omitempty"`
 	TransitionMs int      `json:"transition_ms,omitempty"`
 }
 
-func NewSlideshowParams(imagesURLs []string) *SlideshowParams {
-	return &SlideshowParams{
-		ImagesURLs:   imagesURLs,
-		DurationMs:   1750,
-		TransitionMs: 250,
+func NewSlideshowParams(imagesURLs []string) *VideoParams {
+	return &VideoParams{
+		SlideshowSpec: &SlideshowSpec{
+			ImagesURLs:   imagesURLs,
+			DurationMs:   1750,
+			TransitionMs: 250,
+		},
 	}
 }
 
 // CreateSlideshow creates a slideshow video on the page specified by page id.
-func (c *Client) CreateSlideshow(ctx context.Context, id string, params *SlideshowParams) (string, error) {
+func (c *Client) CreateSlideshow(ctx context.Context, id string, params *VideoParams) (string, error) {
 	var r map[string]string
 	var body bytes.Buffer
 	w := multipart.NewWriter(&body)
-	specJSON, err := json.Marshal(params)
-	if err != nil {
+	if err := writeStructIntoMultipart(w, params); err != nil {
 		w.Close()
-		return "", xerrors.Wrap(err, "could not marshal *SlideshowParams")
-	}
-	if err := w.WriteField("slideshow_spec", string(specJSON)); err != nil {
-		w.Close()
-		return "", xerrors.Wrap(err, "could not write multipart field `slideshow_spec`")
+		return "", xerrors.Wrap(err, "could not write multipart field of *VideoParams")
 	}
 	w.Close()
 	if err := c.PostVideo(ctx, fmt.Sprintf("/%s/videos", id), nil, &MultipartParams{
