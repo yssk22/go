625515e964dc283a71cfd5fcd215333a4dd8c46c
Yohei Sasaki <yohei@fb.com>
2018-11-12 23:15:18 +0900

    don't use the new struct

--- gae/service/builtin/config_api.go
+++ gae/service/builtin/config_api.go
@@ -2,7 +2,6 @@ package builtin
 
 import (
 	"context"
-	"log"
 
 	"github.com/yssk22/go/gae/service"
 	"github.com/yssk22/go/gae/service/config"
@@ -20,20 +19,14 @@ func getConfig(ctx context.Context, key string) (*config.ServiceConfig, error) {
 	return s.Config.Get(ctx, key), nil
 }
 
-type updateConfigParams struct {
-	Value string `json:"value"`
-}
-
 // @api path=/admin/api/configs/:key.json
-func updateConfig(ctx context.Context, key string, newConfig *updateConfigParams) (*config.ServiceConfig, error) {
+func updateConfig(ctx context.Context, key string, newConfig *config.ServiceConfig) (*config.ServiceConfig, error) {
 	s := service.FromContext(ctx)
 	cfg := s.Config.Get(ctx, key)
 	if cfg == nil {
 		return nil, nil
 	}
 	cfg.Value = newConfig.Value
-	log.Println(key, cfg.Value)
 	s.Config.Set(ctx, cfg)
-	log.Println(key, cfg.Value)
 	return cfg, nil
 }
