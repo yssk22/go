107549993dd6fbc6e6ea59becfd1c4b761971b63
Yohei Sasaki <yohei@fb.com>
2018-11-12 16:42:58 +0900

    regenerated

--- generator/flow/example/ServerTypes.js
+++ generator/flow/example/ServerTypes.js
@@ -1,32 +1,29 @@
-
-import * as types from "types"
-
-
+import * as types from 'types';
 
 export type Example = {
-BoolVal: boolean,
-BoolPtr: ?boolean,
-IntVaL: number,
-IntPtr: ?number,
-FloatVal: number,
-FloatPtr: ?number,
-StringVal: string,
-StringPtr: ?string,
-TimeVal: Date,
-TimePtr: ?Date,
-InnerVal: ?Inner,
-InnerPtr: ?Inner,
-Imported: types.RGB,
-}
+  BoolVal: boolean,
+  BoolPtr: ?boolean,
+  IntVaL: number,
+  IntPtr: ?number,
+  FloatVal: number,
+  FloatPtr: ?number,
+  StringVal: string,
+  StringPtr: ?string,
+  TimeVal: Date,
+  TimePtr: ?Date,
+  InnerVal: ?Inner,
+  InnerPtr: ?Inner,
+  Imported: types.RGB
+};
 export type Inner = {
-BoolVal: boolean,
-BoolPtr: ?boolean,
-IntVaL: number,
-IntPtr: ?number,
-FloatVal: number,
-FloatPtr: ?number,
-StringVal: string,
-StringPtr: ?string,
-TimeVal: Date,
-TimePtr: ?Date,
-}
+  BoolVal: boolean,
+  BoolPtr: ?boolean,
+  IntVaL: number,
+  IntPtr: ?number,
+  FloatVal: number,
+  FloatPtr: ?number,
+  StringVal: string,
+  StringPtr: ?string,
+  TimeVal: Date,
+  TimePtr: ?Date
+};
