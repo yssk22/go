fd26ff759da6db47dfb4847d0d9db9b778e55596
yssk22 <yssk22@gmail.com>
2018-11-15 00:54:03 +0900

    use function options rather than method chain to simplify the generated code

--- gae/datastore/query.go
+++ gae/datastore/query.go
@@ -5,44 +5,212 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/yssk22/go/x/xerrors"
 	"github.com/yssk22/go/x/xlog"
 	"google.golang.org/appengine/datastore"
 )
 
-// NewQuery returns an *query configured with options
-func NewQuery(kind string) Query {
-	return &query{
-		kind: kind,
-	}
+// QueryOption is a function to configure query operation
+type QueryOption func(*query) *query
+
+// Ancestor sets the ancestor filter
+func Ancestor(a *datastore.Key) QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.ancestor = a
+		return q
+	})
 }
 
-// Query is an interface to run a query
-type Query interface {
-	// execution functions
+// Eq sets the "=" filter on the `name` field.
+func Eq(name string, value interface{}) QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.filters = append(q.filters, &filter{
+			Name:  name,
+			Type:  filterTypeEq,
+			Value: value,
+		})
+		return q
+	})
+}
 
-	GetAll(context.Context, interface{}) ([]*datastore.Key, error)
-	MustGetAll(context.Context, interface{}) []*datastore.Key
-	Run(context.Context) (*datastore.Iterator, error)
-	MustRun(context.Context) *datastore.Iterator
-	Count(context.Context) (int, error)
-	MustCount(context.Context) int
+// Lt sets the `<` filter on the `name` field.
+func Lt(name string, value interface{}) QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.filters = append(q.filters, &filter{
+			Name:  name,
+			Type:  filterTypeLt,
+			Value: value,
+		})
+		return q
+	})
+}
 
-	// builder functions
-	Ancestor(*datastore.Key) Query
-	KeysOnly(bool) Query
-	Eq(string, interface{}) Query
-	Lt(string, interface{}) Query
-	Le(string, interface{}) Query
-	Gt(string, interface{}) Query
-	Ge(string, interface{}) Query
-	Ne(string, interface{}) Query
-	Asc(string) Query
-	Desc(string) Query
-	Limit(int) Query
-	Start(string) Query
-	End(string) Query
-	WithLogging(key string) Query
+// Le sets the `<=` filter on the `name` field.
+func Le(name string, value interface{}) QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.filters = append(q.filters, &filter{
+			Name:  name,
+			Type:  filterTypeLe,
+			Value: value,
+		})
+		return q
+	})
+}
+
+// Gt sets the `>` filter on the `name` field.
+func Gt(name string, value interface{}) QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.filters = append(q.filters, &filter{
+			Name:  name,
+			Type:  filterTypeGt,
+			Value: value,
+		})
+		return q
+	})
+}
+
+// Ge sets the `>=` filter on the `name` field.
+func Ge(name string, value interface{}) QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.filters = append(q.filters, &filter{
+			Name:  name,
+			Type:  filterTypeGe,
+			Value: value,
+		})
+		return q
+	})
+}
+
+// Ne sets the `!=` filter on the `name` field.
+func Ne(name string, value interface{}) QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.filters = append(q.filters, &filter{
+			Name:  name,
+			Type:  filterTypeNe,
+			Value: value,
+		})
+		return q
+	})
+}
+
+// Asc specifies ascending order on the given filed.
+func Asc(name string) QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.orders = append(q.orders, &order{
+			Name: name,
+			Type: orderTypeAsc,
+		})
+		return q
+	})
+}
+
+// Desc specifies descending order on the given filed.
+func Desc(name string) QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.orders = append(q.orders, &order{
+			Name: name,
+			Type: orderTypeDesc,
+		})
+		return q
+	})
+}
+
+// Limit sets the limit
+func Limit(value int) QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.limit = value
+		return q
+	})
+}
+
+// Start sets the start cursor
+func Start(value string) QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.startCursor = value
+		return q
+	})
+}
+
+// End sets the end cursor
+func End(value string) QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.endCursor = value
+		return q
+	})
+}
+
+// WithLogging sets the logger key for the query
+func WithLogging(loggerKey string) QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.loggerKey = loggerKey
+		return q
+	})
+}
+
+// ViaKeys runs GetAll query to get keys only at first then call GetMulti with these keys to utilize the cache.ViaKeys
+func ViaKeys() QueryOption {
+	return QueryOption(func(q *query) *query {
+		q.viaKeys = true
+		return q
+	})
+}
+
+// GetAll fills the query result into dst and returns corresponding *datastore.Key
+func GetAll(ctx context.Context, kind string, dst interface{}, options ...QueryOption) ([]*datastore.Key, error) {
+	q := &query{
+		kind: kind,
+	}
+	for _, f := range options {
+		q = f(q)
+	}
+	if dst == nil {
+		q.keysOnly = true
+	}
+	if q.viaKeys {
+		q.keysOnly = true
+	}
+	prepared, err := q.prepare(ctx)
+	if err != nil {
+		return nil, err
+	}
+	keys, err := prepared.GetAll(ctx, dst)
+	if err != nil {
+		return nil, err
+	}
+	if dst != nil && q.viaKeys {
+		err = GetMulti(ctx, keys, dst)
+		return keys, err
+	}
+	return keys, err
+}
+
+// Run runs a query and returns *datastore.Iterator
+func Run(ctx context.Context, kind string, options ...QueryOption) (*datastore.Iterator, error) {
+	q := &query{
+		kind: kind,
+	}
+	for _, f := range options {
+		q = f(q)
+	}
+	prepared, err := q.prepare(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return prepared.Run(ctx), nil
+}
+
+// Count returns a count
+func Count(ctx context.Context, kind string, options ...QueryOption) (int, error) {
+	q := &query{
+		kind: kind,
+	}
+	for _, f := range options {
+		q = f(q)
+	}
+	prepared, err := q.prepare(ctx)
+	if err != nil {
+		return 0, err
+	}
+	return prepared.Count(ctx)
 }
 
 type orderType string
@@ -85,169 +253,8 @@ type query struct {
 	endCursor   string
 	limit       int
 	offset      int
-	loggerKey   string `json:"-"`
-}
-
-// Ancestor sets the ancestor filter
-func (q *query) Ancestor(a *datastore.Key) Query {
-	q.ancestor = a
-	return q
-}
-
-// KeysOnly sets the query to fetch only keys.
-func (q *query) KeysOnly(t bool) Query {
-	q.keysOnly = t
-	return q
-}
-
-// Eq sets the "=" filter on the `name` field.
-func (q *query) Eq(name string, value interface{}) Query {
-	q.filters = append(q.filters, &filter{
-		Name:  name,
-		Type:  filterTypeEq,
-		Value: value,
-	})
-	return q
-}
-
-// Lt sets the `<` filter on the `name` field.
-func (q *query) Lt(name string, value interface{}) Query {
-	q.filters = append(q.filters, &filter{
-		Name:  name,
-		Type:  filterTypeLt,
-		Value: value,
-	})
-	return q
-}
-
-// Le sets the `<=` filter on the `name` field.
-func (q *query) Le(name string, value interface{}) Query {
-	q.filters = append(q.filters, &filter{
-		Name:  name,
-		Type:  filterTypeLe,
-		Value: value,
-	})
-	return q
-}
-
-// Gt sets the `>` filter on the `name` field.
-func (q *query) Gt(name string, value interface{}) Query {
-	q.filters = append(q.filters, &filter{
-		Name:  name,
-		Type:  filterTypeGt,
-		Value: value,
-	})
-	return q
-}
-
-// Ge sets the `>=` filter on the `name` field.
-func (q *query) Ge(name string, value interface{}) Query {
-	q.filters = append(q.filters, &filter{
-		Name:  name,
-		Type:  filterTypeGe,
-		Value: value,
-	})
-	return q
-}
-
-// Ne sets the `!=` filter on the `name` field.
-func (q *query) Ne(name string, value interface{}) Query {
-	q.filters = append(q.filters, &filter{
-		Name:  name,
-		Type:  filterTypeNe,
-		Value: value,
-	})
-	return q
-}
-
-// Asc specifies ascending order on the given filed.
-func (q *query) Asc(name string) Query {
-	q.orders = append(q.orders, &order{
-		Name: name,
-		Type: orderTypeAsc,
-	})
-	return q
-}
-
-// Desc specifies descending order on the given filed.
-func (q *query) Desc(name string) Query {
-	q.orders = append(q.orders, &order{
-		Name: name,
-		Type: orderTypeDesc,
-	})
-	return q
-}
-
-// Limit sets the limit
-func (q *query) Limit(value int) Query {
-	q.limit = value
-	return q
-}
-
-// Start sets the start cursor
-func (q *query) Start(value string) Query {
-	q.startCursor = value
-	return q
-}
-
-// End sets the end cursor
-func (q *query) End(value string) Query {
-	q.endCursor = value
-	return q
-}
-
-// WithLogging sets the logger key for the query
-func (q *query) WithLogging(loggerKey string) Query {
-	q.loggerKey = loggerKey
-	return q
-}
-
-// GetAll fills the query result into dst and returns corresponding *datastore.Key
-func (q *query) GetAll(ctx context.Context, dst interface{}) ([]*datastore.Key, error) {
-	prepared, err := q.prepare(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return prepared.GetAll(ctx, dst)
-}
-
-// MustGetAll is like GetAll but panic if an error occurs
-func (q *query) MustGetAll(ctx context.Context, dst interface{}) []*datastore.Key {
-	keys, err := q.GetAll(ctx, dst)
-	xerrors.MustNil(err)
-	return keys
-}
-
-// Run runs a query and returns *datastore.Iterator
-func (q *query) Run(ctx context.Context) (*datastore.Iterator, error) {
-	prepared, err := q.prepare(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return prepared.Run(ctx), nil
-}
-
-// MustRun is like Run but panic if an error occurs
-func (q *query) MustRun(ctx context.Context) *datastore.Iterator {
-	iter, err := q.Run(ctx)
-	xerrors.MustNil(err)
-	return iter
-}
-
-// Count returns a count
-func (q *query) Count(ctx context.Context) (int, error) {
-	prepared, err := q.prepare(ctx)
-	if err != nil {
-		return 0, err
-	}
-	return prepared.Count(ctx)
-}
-
-// MustCount is like Count but panic if an error occurs
-func (q *query) MustCount(ctx context.Context) int {
-	c, err := q.Count(ctx)
-	xerrors.MustNil(err)
-	return c
+	loggerKey   string
+	viaKeys     bool
 }
 
 func (q *query) prepare(ctx context.Context) (*datastore.Query, error) {
