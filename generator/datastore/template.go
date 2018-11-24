a1d65f46b7876ffe4b8c652f115c5540c88ac3bd
yssk22 <yssk22@gmail.com>
2018-11-25 02:09:29 +0900

    implement datastore query generator

--- generator/datastore/template.go
+++ generator/datastore/template.go
@@ -29,35 +29,13 @@ func (f {{.StructName}}ReplacerFunc) Replace(old *{{.StructName}}, new *{{.Struc
 	return f(old, new)
 }
 
-type {{.StructName}}Datastore interface {
-	Get(context.Context, interface{}, ...ds.Option) (*datastore.Key, *{{.StructName}}, error)
-	MustGet(context.Context, interface{}, ...ds.Option) (*datastore.Key, *{{.StructName}})
-	GetMulti(context.Context, interface{}, ...ds.Option) ([]*datastore.Key, []*{{.StructName}}, error)
-	MustGetMulti(context.Context, interface{}, ...ds.Option) ([]*datastore.Key, []*{{.StructName}})
+type {{.StructName}}Kind struct {}
 
-	Put(context.Context, *{{.StructName}}, ...ds.Option) (*datastore.Key, error)
-	MustPut(context.Context, *{{.StructName}}, ...ds.Option) (*datastore.Key)
-	PutMulti(context.Context, []*{{.StructName}}, ...ds.Option) ([]*datastore.Key, error)
-	MustPutMulti(context.Context, []*{{.StructName}}, ...ds.Option) ([]*datastore.Key)
-
-	Delete(context.Context, interface{}, ...ds.Option) (*datastore.Key, error)
-	MustDelete(context.Context, interface{}, ...ds.Option) (*datastore.Key)
-	DeleteMulti(context.Context, interface{}, ...ds.Option) ([]*datastore.Key, error)
-	MustDeleteMulti(context.Context, interface{}, ...ds.Option) ([]*datastore.Key)
-
-	Replace(context.Context, *{{.StructName}}, {{.StructName}}Replacer, ...ds.Option) (*datastore.Key, *{{.StructName}}, error)
-	MustReplace(context.Context, *{{.StructName}}, {{.StructName}}Replacer, ...ds.Option) (*datastore.Key, *{{.StructName}})
-	ReplaceMulti(context.Context, []*{{.StructName}}, {{.StructName}}Replacer, ...ds.Option) ([]*datastore.Key, []*{{.StructName}}, error)
-	MustReplaceMulti(context.Context, []*{{.StructName}}, {{.StructName}}Replacer, ...ds.Option) ([]*datastore.Key, []*{{.StructName}})
+func New{{.StructName}}Kind() *{{.StructName}}Kind {
+	return {{snakecase .StructName}}KindInstance
 }
 
-func Get{{.StructName}}Datastore() {{.StructName}}Datastore {
-	return {{snakecase .StructName}}Instance
-}
-
-type {{snakecase .StructName}}Datastore struct {}
-
-func (d *{{snakecase .StructName}}Datastore) Get(ctx context.Context, key interface{}, options ...ds.Option) (*datastore.Key, *{{.StructName}}, error) {
+func (d *{{.StructName}}Kind) Get(ctx context.Context, key interface{}, options ...ds.CRUDOption) (*datastore.Key, *{{.StructName}}, error) {
     keys, ents, err := d.GetMulti(ctx, []interface{}{key}, options...)
     if err != nil {
         return nil, nil, err
@@ -65,94 +43,37 @@ func (d *{{snakecase .StructName}}Datastore) Get(ctx context.Context, key interf
     return keys[0], ents[0], nil
 }
 
-func (d *{{snakecase .StructName}}Datastore) MustGet(ctx context.Context, key interface{}, options ...ds.Option) (*datastore.Key, *{{.StructName}}) {
+func (d *{{.StructName}}Kind) MustGet(ctx context.Context, key interface{}, options ...ds.CRUDOption) (*datastore.Key, *{{.StructName}}) {
 	k, v, e := d.Get(ctx, key, options...)
 	xerrors.MustNil(e)
 	return k, v
 }
 
-func (d *{{snakecase .StructName}}Datastore) GetMulti(ctx context.Context, keys interface{}, options ...ds.Option) ([]*datastore.Key, []*{{.StructName}}, error) {
+func (d *{{.StructName}}Kind) GetMulti(ctx context.Context, keys interface{}, options ...ds.CRUDOption) ([]*datastore.Key, []*{{.StructName}}, error) {
 	var err error
 	var dsKeys []*datastore.Key
-	var memKeys []string
 	var ents []*{{.StructName}}
-
-	opts := ds.NewCRUDOption(options...)
-	if opts.Namespace != nil {
-		ctx, err = appengine.Namespace(ctx, *(opts.Namespace))
-		if err != nil {
-			return nil, nil, xerrors.Wrap(err, "cannot enforce namespace")
-		}
-	}
-
 	if dsKeys, err = ds.NormalizeKeys(ctx, "{{.StructName}}", keys); err != nil {
 		return nil, nil, xerrors.Wrap(err, "could not normalize keys: %v", keys)
 	}
-
 	size := len(dsKeys)
 	if size == 0 {
 		return nil, nil, nil
 	}
-
 	ents = make([]*{{.StructName}}, size, size)
-	// fetch from cache
-	if !opts.NoCache {
-		memKeys = make([]string, size, size)
-		for i := range dsKeys {
-			memKeys[i] = ds.GetMemcacheKey(dsKeys[i])
-		}
-		err = memcache.GetMulti(ctx, memKeys, ents)
-		if err == nil { 
-			// Hit caches on all keys!!
-			return dsKeys, ents, nil
-		}
+	if err = ds.GetMulti(ctx, dsKeys, ents, options...); err != nil {
+		return nil, nil, err
 	}
-
-	key2Idx := make(map[*datastore.Key]int)
-	cacheMissingKeys := make([]*datastore.Key, 0)
-	for i := range ents {
-		if ents[i] == nil {
-			key2Idx[dsKeys[i]] = i
-			cacheMissingKeys = append(cacheMissingKeys, dsKeys[i])
-		}
-	}
-	cacheMissingSize := len(cacheMissingKeys)
-	cacheMissingEnts := make([]*{{.StructName}}, cacheMissingSize, cacheMissingSize)	
-	if err = ds.GetMulti(ctx, cacheMissingKeys, cacheMissingEnts); ds.IsDatastoreError(err) {
-		// we return nil even some ents hits the cache.
-		return nil, nil, xerrors.Wrap(err, "datastore error")
-	}
-	for i := range cacheMissingKeys {
-		entIdx := key2Idx[cacheMissingKeys[i]]
-		ents[entIdx] = cacheMissingEnts[i]
-	}
-
-	// udpate cache
-	if !opts.NoCache {
-		cacheEnts := make([]*{{.StructName}}, 0)
-		cacheKeys := make([]string, 0)
-		for i := range ents {
-			if ents[i] != nil {
-				cacheEnts = append(cacheEnts, ents[i])
-				cacheKeys = append(cacheKeys, memKeys[i])
-			}
-		}
-		if len(cacheEnts) > 0 {
-			if err := memcache.SetMulti(ctx, cacheKeys, cacheEnts); err != nil {
-			}
-		}
-	}
-
 	return dsKeys, ents, nil
 }
 
-func (d *{{snakecase .StructName}}Datastore) MustGetMulti(ctx context.Context, keys interface{}, options ...ds.Option) ([]*datastore.Key, []*{{.StructName}}) {
+func (d *{{.StructName}}Kind) MustGetMulti(ctx context.Context, keys interface{}, options ...ds.CRUDOption) ([]*datastore.Key, []*{{.StructName}}) {
 	k, v, e := d.GetMulti(ctx, keys, options...)
 	xerrors.MustNil(e)
 	return k, v
 }
 
-func (d *{{snakecase .StructName}}Datastore) Put(ctx context.Context, ent *{{.StructName}}, options ...ds.Option) (*datastore.Key, error) {
+func (d *{{.StructName}}Kind) Put(ctx context.Context, ent *{{.StructName}}, options ...ds.CRUDOption) (*datastore.Key, error) {
 	keys, err := d.PutMulti(ctx, []*{{.StructName}}{ent}, options...)
     if err != nil {
         return nil, err
@@ -160,64 +81,36 @@ func (d *{{snakecase .StructName}}Datastore) Put(ctx context.Context, ent *{{.St
 	return keys[0], nil
 }
 
-func (d *{{snakecase .StructName}}Datastore) MustPut(ctx context.Context, ent *{{.StructName}}, options ...ds.Option) (*datastore.Key) {
+func (d *{{.StructName}}Kind) MustPut(ctx context.Context, ent *{{.StructName}}, options ...ds.CRUDOption) (*datastore.Key) {
 	k, e := d.Put(ctx, ent, options...)
 	xerrors.MustNil(e)
 	return k
 }
 
-func (d *{{snakecase .StructName}}Datastore) PutMulti(ctx context.Context, ents []*{{.StructName}}, options ...ds.Option) ([]*datastore.Key, error) {
+func (d *{{.StructName}}Kind) PutMulti(ctx context.Context, ents []*{{.StructName}}, options ...ds.CRUDOption) ([]*datastore.Key, error) {
 	var err error
 	var size = len(ents)
 	var dsKeys []*datastore.Key
-	if size == 0 {
-		return nil, nil
-	}
-	if size >= ds.MaxEntitiesPerUpdate {
-		return nil, ds.ErrTooManyEnts
-	}
-	opts := ds.NewCRUDOption(options...)
-	if opts.Namespace != nil {
-		ctx, err = appengine.Namespace(ctx, *(opts.Namespace))
-		if err != nil {
-			return nil, xerrors.Wrap(err, "cannot enforce namespace")
-		}
-	}
-
 	dsKeys = make([]*datastore.Key, size, size)
 	for i := range ents {
 		dsKeys[i] = ents[i].NewKey(ctx)
+		{{with .TimestampField -}}
+		ents[i].{{.}} = xtime.Now()
+		{{end -}}
 	}
-
-	if !opts.NoTimestampUpdate {
-		for i := range ents {
-			ents[i].{{.TimestampField}} = xtime.Now()
-		}
+	if dsKeys, err = ds.PutMulti(ctx, dsKeys, ents); err != nil {
+		return nil, err
 	}
-
-	if _, err = ds.PutMulti(ctx, dsKeys, ents); ds.IsDatastoreError(err) {
-		return nil, xerrors.Wrap(err, "datastore error")
-	}
-
-	if !opts.NoCache {
-		memKeys := make([]string, size, size)
-		for i := range memKeys {
-			memKeys[i] = ds.GetMemcacheKey(dsKeys[i])
-		}
-		if err := memcache.DeleteMulti(ctx, memKeys); memcache.IsMemcacheError(err) {
-		}
-	}
-
 	return dsKeys, nil
 }
 
-func (d *{{snakecase .StructName}}Datastore) MustPutMulti(ctx context.Context, ents []*{{.StructName}}, options ...ds.Option) ([]*datastore.Key) {
+func (d *{{.StructName}}Kind) MustPutMulti(ctx context.Context, ents []*{{.StructName}}, options ...ds.CRUDOption) ([]*datastore.Key) {
 	keys, err := d.PutMulti(ctx, ents, options...)
 	xerrors.MustNil(err)
 	return keys
 }
 
-func (d *{{snakecase .StructName}}Datastore) Delete(ctx context.Context, key interface{}, options ...ds.Option) (*datastore.Key, error) {
+func (d *{{.StructName}}Kind) Delete(ctx context.Context, key interface{}, options ...ds.CRUDOption) (*datastore.Key, error) {
     keys, err := d.DeleteMulti(ctx, []interface{}{key}, options...)
     if err != nil {
         return nil, err
@@ -225,61 +118,53 @@ func (d *{{snakecase .StructName}}Datastore) Delete(ctx context.Context, key int
     return keys[0], nil
 }
 
-func (d *{{snakecase .StructName}}Datastore) MustDelete(ctx context.Context, key interface{}, options ...ds.Option) (*datastore.Key) {
+func (d *{{.StructName}}Kind) MustDelete(ctx context.Context, key interface{}, options ...ds.CRUDOption) (*datastore.Key) {
 	k, e := d.Delete(ctx, key, options...)
 	xerrors.MustNil(e)
 	return k
 }
 
-func (d *{{snakecase .StructName}}Datastore) DeleteMulti(ctx context.Context, keys interface{}, options ...ds.Option) ([]*datastore.Key, error) {
+func (d *{{.StructName}}Kind) DeleteMulti(ctx context.Context, keys interface{}, options ...ds.CRUDOption) ([]*datastore.Key, error) {
 	var err error
 	var dsKeys []*datastore.Key
-
-	opts := ds.NewCRUDOption(options...)
-	if opts.Namespace != nil {
-		ctx, err = appengine.Namespace(ctx, *(opts.Namespace))
-		if err != nil {
-			return nil, xerrors.Wrap(err, "cannot enforce namespace")
-		}
-	}
-
 	if dsKeys, err = ds.NormalizeKeys(ctx, "{{.StructName}}", keys); err != nil {
 		return nil, xerrors.Wrap(err, "could not normalize keys: %v", keys)
 	}
-
 	size := len(dsKeys)
 	if size == 0 {
 		return nil, nil
 	}
-	if size >= ds.MaxEntitiesPerUpdate {
-		return nil, ds.ErrTooManyEnts
-	}
-
-	if err = ds.DeleteMulti(ctx, dsKeys); ds.IsDatastoreError(err) {
-		// we return nil even some ents hits the cache.
+	if err = ds.DeleteMulti(ctx, dsKeys); err != nil  {
 		return nil, xerrors.Wrap(err, "datastore error")
 	}
-
-	// invalidate cache
-	if !opts.NoCache {
-		memKeys := make([]string, size, size)
-		for i := range memKeys {
-			memKeys[i] = ds.GetMemcacheKey(dsKeys[i])
-		}
-		if err = memcache.DeleteMulti(ctx, memKeys); memcache.IsMemcacheError(err) {
-		}
-	}
-
 	return dsKeys, nil
 }
 
-func (d *{{snakecase .StructName}}Datastore) MustDeleteMulti(ctx context.Context, keys interface{}, options ...ds.Option) ([]*datastore.Key) {
+func (d *{{.StructName}}Kind) MustDeleteMulti(ctx context.Context, keys interface{}, options ...ds.CRUDOption) ([]*datastore.Key) {
 	k, e := d.DeleteMulti(ctx, keys, options...)
 	xerrors.MustNil(e)
 	return k
 }
 
-func (d *{{snakecase .StructName}}Datastore) Replace(ctx context.Context, ent *{{.StructName}}, replacer {{.StructName}}Replacer, options ...ds.Option) (*datastore.Key, *{{.StructName}}, error) {
+func (d *{{.StructName}}Kind) DeleteMatched(ctx context.Context, q *{{.StructName}}Query, options ...ds.CRUDOption) ([]*datastore.Key, error) {
+	keys, err := q.query.KeysOnly().GetAll(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	_, err = d.DeleteMulti(ctx, keys, options...)
+	if err != nil {
+		return nil, err
+	}
+	return keys, nil
+}
+
+func (d *{{.StructName}}Kind) MustDeleteMatched(ctx context.Context, q *{{.StructName}}Query, options ...ds.CRUDOption) ([]*datastore.Key) {
+	keys, err := d.DeleteMatched(ctx, q, options...)
+	xerrors.MustNil(err)
+	return keys
+}
+
+func (d *{{.StructName}}Kind) Replace(ctx context.Context, ent *{{.StructName}}, replacer {{.StructName}}Replacer, options ...ds.CRUDOption) (*datastore.Key, *{{.StructName}}, error) {
     keys, ents, err := d.ReplaceMulti(ctx, []*{{.StructName}}{ent}, replacer, options...)
     if err != nil {
         return nil, ents[0], err
@@ -287,13 +172,13 @@ func (d *{{snakecase .StructName}}Datastore) Replace(ctx context.Context, ent *{
     return keys[0], ents[0], err
 }
 
-func (d *{{snakecase .StructName}}Datastore) MustReplace(ctx context.Context, ent *{{.StructName}}, replacer {{.StructName}}Replacer, options ...ds.Option) (*datastore.Key, *{{.StructName}}) {
+func (d *{{.StructName}}Kind) MustReplace(ctx context.Context, ent *{{.StructName}}, replacer {{.StructName}}Replacer, options ...ds.CRUDOption) (*datastore.Key, *{{.StructName}}) {
 	k, v, e := d.Replace(ctx, ent, replacer, options...)
 	xerrors.MustNil(e)
 	return k, v
 }
 
-func (d *{{snakecase .StructName}}Datastore) ReplaceMulti(ctx context.Context, ents []*{{.StructName}}, replacer {{.StructName}}Replacer, options ...ds.Option) ([]*datastore.Key, []*{{.StructName}}, error) {
+func (d *{{.StructName}}Kind) ReplaceMulti(ctx context.Context, ents []*{{.StructName}}, replacer {{.StructName}}Replacer, options ...ds.CRUDOption) ([]*datastore.Key, []*{{.StructName}}, error) {
 	var size = len(ents)
 	var dsKeys = make([]*datastore.Key, size, size)
 	if size == 0 {
@@ -315,13 +200,184 @@ func (d *{{snakecase .StructName}}Datastore) ReplaceMulti(ctx context.Context, e
 	return dsKeys, ents, err
 }
 
-func (d *{{snakecase .StructName}}Datastore) MustReplaceMulti(ctx context.Context, ents []*{{.StructName}}, replacer {{.StructName}}Replacer, options ...ds.Option) ([]*datastore.Key, []*{{.StructName}}) {
+func (d *{{.StructName}}Kind) MustReplaceMulti(ctx context.Context, ents []*{{.StructName}}, replacer {{.StructName}}Replacer, options ...ds.CRUDOption) ([]*datastore.Key, []*{{.StructName}}) {
 	k, v, e := d.ReplaceMulti(ctx, ents, replacer, options...)
 	xerrors.MustNil(e)
 	return k, v
 }
 
-var {{snakecase .StructName}}Instance = &{{snakecase .StructName}}Datastore{}
+type {{.StructName}}Query struct {
+	query *ds.Query
+	viaKeys bool
+}
+
+func New{{.StructName}}Query() *{{.StructName}}Query {
+	return &{{.StructName}}Query{
+		query: ds.NewQuery("{{.KindName}}"),
+		viaKeys: false,
+	}
+}
+
+{{queryFuncs .}}
+
+func (d *{{.StructName}}Query) Start(s string) *{{.StructName}}Query {
+	d.query = d.query.Start(s)
+	return d
+}
+
+func (d *{{.StructName}}Query) End(s string) *{{.StructName}}Query {
+	d.query = d.query.End(s)
+	return d
+}
+
+func (d *{{.StructName}}Query) Limit(n int) *{{.StructName}}Query {
+	d.query = d.query.Limit(n)
+	return d
+}
+
+func (d *{{.StructName}}Query) ViaKeys() *{{.StructName}}Query {
+	d.viaKeys = true
+	return d
+}
+
+func (d *{{.StructName}}Query) GetAll(ctx context.Context) ([]*datastore.Key, []{{.StructName}}, error) {
+	if d.viaKeys {
+		keys, err := d.query.KeysOnly().GetAll(ctx, nil)
+		if err != nil {
+			return nil, nil, err
+		}
+		_, ents, err := {{snakecase .StructName}}KindInstance.GetMulti(ctx, keys)
+		if err != nil {
+			return nil, nil, err
+		}
+		list := make([]{{.StructName}}, len(ents))
+		for i, e := range ents {
+			list[i] = *e
+		}
+		return keys, list, nil
+	}
+	var ent []{{.StructName}}
+	keys, err := d.query.GetAll(ctx, &ent)
+	if err != nil {
+		return nil, nil, err
+	}
+	return keys, ent, nil
+}
+
+func (d *{{.StructName}}Query) MustGetAll(ctx context.Context) ([]*datastore.Key, []{{.StructName}}) {
+	keys, ents, err := d.GetAll(ctx)
+	xerrors.MustNil(err)
+	return keys, ents
+}
+
+func (d *{{.StructName}}Query) Count(ctx context.Context) (int, error) {
+	return d.query.Count(ctx)
+}
+
+func (d *{{.StructName}}Query) MustCount(ctx context.Context) (int) {
+	c, err := d.query.Count(ctx)
+	xerrors.MustNil(err)
+	return c
+}
+
+func (d *{{.StructName}}Query) Run(ctx context.Context) (*{{.StructName}}Iterator, error) {
+	iter, err := d.query.Run(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &{{.StructName}}Iterator{
+		ctx: ctx,
+		iter: iter,
+		viaKeys: d.viaKeys,
+	}, err
+}
+
+func (d *{{.StructName}}Query) MustRun(ctx context.Context) (*{{.StructName}}Iterator) {
+	iter, err := d.Run(ctx)
+	xerrors.MustNil(err)
+	return iter
+}
+
+func (d *{{.StructName}}Query) RunAll(ctx context.Context) ([]datastore.Key, []{{.StructName}}, string, error) {
+	iter, err := d.Run(ctx)
+	if err != nil {
+		return nil, nil, "", err
+	}
+	var keys []datastore.Key
+	var ents []{{.StructName}}
+	for {
+		key, ent, err := iter.Next()
+		if err != nil{
+			return nil, nil, "", err
+		}
+		if ent == nil {
+			cursor, err := iter.iter.Cursor()
+			if err != nil {
+				return nil, nil, "", err
+			}
+			return keys, ents, cursor.String(), nil
+		}
+		keys = append(keys, *key)
+		ents = append(ents, *ent)
+	}
+}
+
+func (d *{{.StructName}}Query) MustRunAll(ctx context.Context) ([]datastore.Key, []{{.StructName}}, string) {
+	keys, ents, next, err := d.RunAll(ctx)
+	xerrors.MustNil(err)
+	return keys, ents, next
+}
+
+type {{.StructName}}Iterator struct {
+	ctx context.Context
+	iter *datastore.Iterator
+	viaKeys bool
+}
+
+func (iter *{{.StructName}}Iterator) Cursor() (datastore.Cursor, error) {
+	return iter.iter.Cursor()
+}
+
+func (iter *{{.StructName}}Iterator) MustCursor() (datastore.Cursor) {
+	c, err := iter.iter.Cursor()
+	xerrors.MustNil(err)
+	return c
+}
+
+func (iter *{{.StructName}}Iterator) Next() (*datastore.Key, *{{.StructName}}, error) {
+	if iter.viaKeys {
+		key, err := iter.iter.Next(nil)
+		if err != nil {
+			if err == datastore.Done {
+				return nil, nil, nil
+			}
+			return nil, nil, err
+		}
+		_, ent, err := {{snakecase .StructName}}KindInstance.Get(iter.ctx, key)
+		if err != nil {
+			return nil, nil, err
+		}
+		return key, ent, nil
+
+	}
+	var ent {{.StructName}}
+	key, err := iter.iter.Next(&ent)
+	if err != nil {
+		if err == datastore.Done {
+			return nil, nil, nil
+		}
+		return nil, nil, err
+	}
+	return key, &ent, nil
+}
+
+func (iter *{{.StructName}}Iterator) MustNext() (*datastore.Key, *{{.StructName}}) {
+	key, ent, err := iter.Next()
+	xerrors.MustNil(err)
+	return key, ent
+}
+
+var {{snakecase .StructName}}KindInstance = &{{.StructName}}Kind{}
 
 {{end -}}
 `
