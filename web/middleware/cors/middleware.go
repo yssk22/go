c4b18e87745d9e371e2dd07c05d64ec09bc09bd8
yssk22 <yssk22@gmail.com>
2019-06-17 14:30:19 +0900

    added preflight handler

--- web/middleware/cors/middleware.go
+++ web/middleware/cors/middleware.go
@@ -1,25 +1,60 @@
 package cors
 
 import (
-	"log"
-
 	"github.com/yssk22/go/web"
 	"github.com/yssk22/go/web/response"
 )
 
-type Middleware struct {
-	AllowOrigin string
+type middleware struct {
+	m         map[string]bool
+	all       bool
+	preflight web.Handler
 }
 
-func NewMiddleware(allowOrigin string) *Middleware {
-	return &Middleware{
-		AllowOrigin: allowOrigin,
+var defaultPreflight = web.HandlerFunc(func(req *web.Request, next web.NextHandler) *response.Response {
+	resp := response.NewText("OK")
+	return resp
+})
+
+// NewMiddleware returns a web.Handler interface for CORS support
+func NewMiddleware(origins ...string) web.Handler {
+	return NewMiddlewareWithPreflight(defaultPreflight, origins...)
+}
+
+// NewMiddlewareWithPreflight returns a web.Handler interface for CORS support
+// by specifying the preflight request handler
+func NewMiddlewareWithPreflight(preflight web.Handler, origins ...string) web.Handler {
+	var m = &middleware{
+		m:         make(map[string]bool),
+		all:       false,
+		preflight: preflight,
 	}
+	for _, o := range origins {
+		m.m[o] = true
+	}
+	if _, ok := m.m["*"]; ok {
+		m.all = true
+	}
+	return m
 }
 
-func (m *Middleware) Process(req *web.Request, next web.NextHandler) *response.Response {
-	log.Println("New request")
-	resp := next(req)
-	resp.Header.Add("Access-Control-Allow-Origin", m.AllowOrigin)
+func (m *middleware) Process(req *web.Request, next web.NextHandler) *response.Response {
+	var resp *response.Response
+	if req.Method == "OPTIONS" {
+		resp = m.preflight.Process(req, next)
+		resp.Header.Add("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,HEAD,OPTIONS")
+	} else {
+		resp = next(req)
+	}
+	if m.all {
+		resp.Header.Add("Access-Control-Allow-Origin", "*")
+	} else {
+		origin := req.Request.Header.Get("Origin")
+		if origin != "" {
+			if _, ok := m.m[origin]; ok {
+				resp.Header.Add("Access-Control-Allow-Origin", origin)
+			}
+		}
+	}
 	return resp
 }
