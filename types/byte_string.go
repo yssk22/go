dec310b849299c238c14adcaec4ab54d430bbe9e
yssk22 <yssk22@gmail.com>
2019-12-02 11:48:33 +0900

    fix ByteString marshalization

--- types/byte_string.go
+++ types/byte_string.go
@@ -1,20 +1,23 @@
 package types
 
-import "fmt"
+import (
+	"encoding/json"
+)
 
 // ByteString is an alias type of []byte to avoid base64 encoding in JSON.
 type ByteString []byte
 
 // MarshalJSON implements json.Marshaler#MarshalJSON()
 func (bs ByteString) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf("\"%s\"", string(bs))), nil
+	return json.Marshal(string(bs))
 }
 
 // UnmarshalJSON implements json.Unmarshaler#UnmarshalJSON()
 func (bs *ByteString) UnmarshalJSON(b []byte) error {
-	if b[0] != '"' || b[len(b)-1] != '"' {
-		return fmt.Errorf("Invalid string")
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
 	}
-	*bs = b[1 : len(b)-1]
+	*bs = []byte(s)
 	return nil
 }
