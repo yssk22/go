236d182e442e89784e261a97ccbc91aa43646776
yssk22 <yssk22@gmail.com>
2020-01-31 14:23:43 +0900

    deprecate gae

--- gae/taskqueue/wrapper.go
+++
@@ -1,9 +0,0 @@
-package taskqueue
-
-import "google.golang.org/appengine/taskqueue"
-
-// named queue is not supported in aetest environment,
-// So function can be configurable for testing purose.
-
-// Add is a wapper for google.golang.org/appengine/taskqueue.Add
-var Add = taskqueue.Add
